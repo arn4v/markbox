95d23062984559ae9a8accade9a0b3d81eb83192
Arnav Gosain <arnav@arnavgosain.com>
2021-06-15 21:09:28 +0530

    Remove commented out code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,10 @@ import "~/styles/index.css";
 import "inter-ui/inter.css";
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-} from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  // link: new HttpLink({
-  //   uri: "/api/graphql",
-  //   credentials: "same-origin",
-  // }),
   uri: "/api/graphql",
   ssrMode: false,
 });
