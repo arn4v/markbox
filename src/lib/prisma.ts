88794b70bdcbf78e67bd135c5665bf5eaddb01b6
Arnav Gosain <arnav@arnavgosain.com>
2021-06-25 20:36:16 +0530

    Add environment variable types

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,8 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { isProd } from "~/constants";
 
 const options: Prisma.PrismaClientOptions = {};
-if (!isProd) options.log = ["query", "info", `warn`, `error`];
+if (!isProd && !!process.env.PRISMA_LOG)
+	options.log = ["query", "info", `warn`, `error`];
 
 if (!global.prisma) global.prisma = new PrismaClient(options);
 
--- types/node.d.ts
+++ types/node.d.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  declare module NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
+	declare module NodeJS {
+		interface Global {
+			prisma: PrismaClient;
+		}
+
+		interface ProcessEnv {
+			JWT_SECRET: string;
+			PRISMA_LOG: string;
+		}
+	}
 }
