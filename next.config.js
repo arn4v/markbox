9601b272035028ce53a1e8cca2df4f7c8852bb14
Arnav Gosain <arnav@arnavgosain.com>
2021-06-28 16:50:15 +0530

    Add PWA support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  webpack: (config) => {
-    return config;
-  },
+const withPWA = require("next-pwa");
+
+let config = {
+	webpack: (config) => {
+		return config;
+	},
 };
+
+config = withPWA(
+	Object.assign(config, {
+		pwa: {
+			dest: "public",
+			disable: process.env.NODE_ENV !== "production",
+		},
+	}),
+);
+
+module.exports = config;
