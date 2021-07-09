2d5fedc12d086dca7de4233301e45cb985616177
Arnav Gosain <arnav@arnavgosain.com>
2021-07-09 18:02:00 +0530

    Link correct favicon file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
 		<>
 			<Toaster />
 			<Head>
-				<link rel="shortcut icon" href="/favicon.ico" />
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="manifest" href="/manifest.json" />
 				<link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
