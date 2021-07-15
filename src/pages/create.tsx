444836a0cd7102da8535f4ac31348d886ef4b059
Arnav Gosain <arnav@arnavgosain.com>
2021-07-15 21:28:26 +0530

    Refactor create title/url params logic

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,4 +1,3 @@
-import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 import LoadingPage from "~/components/LoadingPage";
@@ -7,26 +6,30 @@ import useIsPwa from "~/hooks/use-pwa";
 import { CreateForm } from "~/modules/common/components/Create";
 import Navbar from "~/modules/dashboard/components/Navbar";
 
-interface Props {
-	title?: string;
-	url?: string;
-}
-
-const CreatePage = ({}: Props) => {
+const CreatePage = () => {
 	const router = useRouter();
 	const isPwa = useIsPwa();
-	const {
-		title = "",
-		url = "",
-		popup = false,
-	} = router.query as unknown as {
-		title?: string;
-		url?: string;
-		popup?: boolean;
-	};
+	const [{ title, url, popup }, setState] = React.useState({
+		title: null,
+		url: null,
+		description: null,
+		popup: false,
+	});
 	const { isLoading } = useAuth(true);
+	const [isMounted, setMounted] = React.useState(false);
 
-	if (isLoading) return <LoadingPage />;
+	React.useEffect(() => {
+		// Decode window.location query params
+		const { searchParams } = new URL(window.location.href);
+		const title = searchParams.get("title");
+		const url = searchParams.get("url");
+		const description = searchParams.get("description");
+		const popup = !!searchParams.get("popup") ?? false;
+		setState({ title, url, description, popup });
+		setMounted(true);
+	}, []);
+
+	if (isLoading || !isMounted) return <LoadingPage />;
 
 	return (
 		<div className="flex flex-col w-screen min-h-screen scrollbar-track-gray-100 scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-800 scrollbar scrollbar-thin bg-white dark:bg-black items-center">
@@ -40,7 +43,7 @@ const CreatePage = ({}: Props) => {
 			<div className="flex flex-col gap-6 p-3 w-11/12 lg:p-6 dark:bg-gray-900 bg-white rounded-md border border-gray-300 lg:w-1/2 shadow-lg mt-8">
 				<CreateForm
 					onSuccess={() => {
-						if (popup && !isPwa) {
+						if (popup || isPwa) {
 							window.close();
 						} else {
 							router.push("/dashboard");
@@ -52,16 +55,6 @@ const CreatePage = ({}: Props) => {
 			</div>
 		</div>
 	);
-};
-
-export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
-	return {
-		props: {
-			title: (ctx?.query?.title as string) ?? "",
-			url: (ctx?.query?.url as string) ?? "",
-		},
-		notFound: false,
-	};
 };
 
 export default CreatePage;
