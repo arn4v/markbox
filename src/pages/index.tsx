ab28bda90a9c092c7b16180b4cd90139a7fce4d7
Arnav Gosain <arnav@arnavgosain.com>
2021-07-20 17:26:26 +0530

    Extract GetStartedButton into component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,60 @@
+import clsx from "clsx";
 import Link from "next/link";
 import React from "react";
-import { HiArrowRight, HiMenu, HiX } from "react-icons/hi";
+import {
+	HiArrowRight,
+	HiMenu,
+	HiOutlineDocumentText,
+	HiX,
+} from "react-icons/hi";
 import { Typewriter } from "react-simple-typewriter";
 import { Logo } from "~/components/Logo";
 import { useAuth } from "~/hooks/use-auth";
+import useBreakpoints from "~/hooks/use-breakpoints";
 import useDisclosure from "~/hooks/use-disclosure";
+
+const GetStartedButton = ({ className = "" }) => {
+	const { isAuthenticated } = useAuth();
+
+	return isAuthenticated ? (
+		<>
+			<Link href="/dashboard">
+				<a
+					className={clsx(
+						"flex items-center justify-center px-2 lg:px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 gap-2",
+						className,
+					)}
+				>
+					Dashboard <HiArrowRight />
+				</a>
+			</Link>
+		</>
+	) : (
+		<>
+			<a
+				className={clsx(
+					"items-center px-2 w-full flex items-center justify-center lg:px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 gap-2",
+					className,
+				)}
+				href="/api/auth/login"
+			>
+				Get started
+				<HiArrowRight />
+			</a>
+		</>
+	);
+};
 
 export default function IndexPage() {
 	const { isAuthenticated } = useAuth();
 	const { isOpen, onToggle } = useDisclosure();
+	const { isLg } = useBreakpoints();
+
+	React.useEffect(() => {
+		if (isLg && isOpen) {
+			onToggle();
+		}
+	}, [isLg, isOpen, onToggle]);
 
 	return (
 		<div className="min-h-screen w-screen dark:bg-black relative">
@@ -28,25 +74,7 @@ export default function IndexPage() {
 								Docs
 							</a>
 						</Link>
-						{isAuthenticated ? (
-							<>
-								<Link href="/dashboard">
-									<a className="flex items-center justify-center px-2 lg:px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 gap-2">
-										Dashbard <HiArrowRight />
-									</a>
-								</Link>
-							</>
-						) : (
-							<>
-								<a
-									className="items-center px-2 hidden lg:flex lg:px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 gap-2"
-									href="/api/auth/login"
-								>
-									Get started
-									<HiArrowRight />
-								</a>
-							</>
-						)}
+						<GetStartedButton className="hidden lg:flex" />
 					</nav>
 				</div>
 				{isOpen ? (
@@ -59,25 +87,20 @@ export default function IndexPage() {
 										href="/api/auth/login"
 									>
 										Docs
+										<HiOutlineDocumentText />
 									</a>
 								</Link>
 							</li>
 							<li className="px-6 mt-4">
-								<a
-									className="flex items-center justify-center px-2 lg:px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 gap-2"
-									href="/api/auth/login"
-								>
-									Get started
-									<HiArrowRight />
-								</a>
+								<GetStartedButton />
 							</li>
 						</ul>
 					</div>
 				) : null}
 			</header>
 			<div className="w-96 h-64 blur-[350px] absolute bg-sky-400 rounded-full top-1/3 left-1/2 -translate-x-1/2 transform"></div>
-			<section className="dark:text-blueGray-200 text-blueGray-700 container flex flex-col px-5 py-8 lg:py-24 mx-auto items-center">
-				<div className="flex flex-col w-full mb-12 text-left lg:text-center">
+			<section className="dark:text-blueGray-200 text-blueGray-700 container flex flex-col gap-12 px-5 py-8 lg:py-24 mx-auto items-center">
+				<div className="flex flex-col w-full text-left lg:text-center">
 					<h2 className="mb-4 text-xs font-semibold tracking-widest text-black dark:text-white uppercase text-center">
 						API-first bookmarking for Developers
 					</h2>
