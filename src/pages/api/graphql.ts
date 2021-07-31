54d7d820c3dbc2f6df1f0bca75a8bd39fd23cde9
Arnav Gosain <arnav@arnavgosain.com>
2021-07-31 23:04:16 +0530

    build: downgrade apollo-server-micro

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,10 +1,12 @@
 import { ApolloServer } from "apollo-server-micro";
+import { isProd } from "~/config";
 import schema from "~/graphql/schema";
 import { createHandler, prisma, withCookies } from "~/lib/utils.server";
 import GQLContext from "~/types/GQLContext";
 
 const apolloServer = new ApolloServer({
 	schema: schema,
+	playground: !isProd,
 	context: ({ req, res }: GQLContext) => {
 		return { req, res, prisma };
 	},
