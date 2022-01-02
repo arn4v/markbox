f1eae38f4f3c58eba459a36a59cae85f454e3bad
Arnav Gosain <arnav@arnavgosain.com>
2022-01-02 20:34:48 +0530

    Filter by params instead of route

--- src/modules/dashboard/components/Tag.tsx
+++ src/modules/dashboard/components/Tag.tsx
@@ -67,7 +67,14 @@ export default function Tag({
 			data-test="dashboard-tag"
 			className={clsx(["flex w-full gap-4 items-center"])}
 		>
-			<Link href={`/tag/${data?.name}`}>
+			<Link
+				href={{
+					href: "/app",
+					query: {
+						tag: data?.name,
+					},
+				}}
+			>
 				<a
 					className={clsx([
 						"px-4 py-2 flex-grow transition rounded-md dark:text-white border flex items-center justify-between",
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,9 +1,17 @@
 import { NextSeo } from "next-seo";
+import * as React from "react";
+import { useRouter } from "next/router";
 import LoadingPage from "~/components/LoadingPage";
 import { useAuth } from "~/hooks/use-auth";
 import { AppLayout } from "~/modules/dashboard/layouts/App.layout";
 
 const DashboardPage = () => {
+	const router = useRouter();
+
+	const tag = React.useMemo(() => {
+		return (router.query?.tag as string) ?? "All";
+	}, [router.query?.tag]);
+
 	const { isLoading, isAuthenticated } = useAuth(true);
 
 	if (isLoading || !isAuthenticated) return <LoadingPage />;
@@ -11,7 +19,7 @@ const DashboardPage = () => {
 	return (
 		<>
 			<NextSeo title="Dashboard" noindex />
-			<AppLayout tag="All" />
+			<AppLayout tag={tag} />
 		</>
 	);
 };
